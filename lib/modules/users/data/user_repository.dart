503240a7afb1728e07445a6f87fb46f61dabd701
MatheusTrinca <matheusfelipetrinca@gmail.com>
2020-10-02 00:26:01 -0300

    login user e finalizado user

--- lib/modules/users/data/user_repository.dart
+++ lib/modules/users/data/user_repository.dart
@@ -1,14 +1,15 @@
 import 'package:injectable/injectable.dart';
 import 'package:mysql1/mysql1.dart';
 import 'package:pizza_delivery_api/application/database/i_database_connection.dart';
+import 'package:pizza_delivery_api/application/entities/user.dart';
 import 'package:pizza_delivery_api/application/exceptions/database_error_exception.dart';
+import 'package:pizza_delivery_api/application/exceptions/user_not_found_exception.dart';
 import 'package:pizza_delivery_api/application/helpers/cripty_helpers.dart';
 import 'package:pizza_delivery_api/modules/users/data/i_user_repository.dart';
 import 'package:pizza_delivery_api/modules/users/view_models/register_user_input_model.dart';
 
 @LazySingleton(as: IUserRepository)
-class UserRepository implements IUserRepository{
-
+class UserRepository implements IUserRepository {
   final IDatabaseConnection _connection;
 
   UserRepository(this._connection);
@@ -16,22 +17,46 @@ class UserRepository implements IUserRepository{
   @override
   Future<void> saveUser(RegisterUserInputModel inputModel) async {
     final conn = await _connection.openConnection();
-    try{
-      await conn.query(
-        'insert usuario(nome, email, senha) values(?, ?, ?)',
-        [
-          inputModel.name,
-          inputModel.email,
-          inputModel.password
-        ]
-      );
-
-    }on MySqlConnection catch(e){
+    try {
+      await conn.query('insert usuario(nome, email, senha) values(?, ?, ?)', [
+        inputModel.name,
+        inputModel.email,
+        CriptyHelpers.generateSHA256Hash(inputModel.password)
+      ]);
+    } on MySqlConnection catch (e) {
       print(e);
       throw DatabaseErrorException(message: 'Erro ao registrar usuário');
-    }finally{
+    } finally {
       await conn?.close();
     }
   }
 
+  @override
+  Future<User> login(String email, String password) async {
+    final conn = await _connection.openConnection();
+    try {
+      final result = await conn.query('select * from usuario where email = ? and senha = ?',
+        [email, CriptyHelpers.generateSHA256Hash(password)],
+      );
+
+      if(result.isEmpty){
+        throw UserNotFoundException();
+      }
+
+      final fields = result.first.fields;
+
+      return User (
+        id: fields['id_usuario'] as int,
+        name: fields['nome'] as String,
+        email: fields['email'] as String,
+        password: fields['password'] as String
+      );
+
+    } on MySqlConnection catch (e) {
+      print(e);
+      throw DatabaseErrorException(message: 'Erro ao buscar usuário');
+    } finally {
+      await conn?.close();
+    }
+  }
 }
