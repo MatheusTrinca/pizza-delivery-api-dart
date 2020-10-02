abc3db361adefc549675ca5badae8a1775b48967
MatheusTrinca <matheusfelipetrinca@gmail.com>
2020-10-02 15:27:09 -0300

    finalizado user

--- lib/application/routers/routers_configure.dart
+++ lib/application/routers/routers_configure.dart
@@ -1,4 +1,5 @@
 import 'package:pizza_delivery_api/application/routers/i_router_configure.dart';
+import 'package:pizza_delivery_api/modules/menu/menu_routers.dart';
 import 'package:pizza_delivery_api/modules/users/users_routers.dart';
 import 'package:pizza_delivery_api/pizza_delivery_api.dart';
 
@@ -6,7 +7,8 @@ import 'package:pizza_delivery_api/pizza_delivery_api.dart';
 class RoutersConfigure{
   final Router _router;
   final List<IRouterConfigure> routers = [
-    UsersRouters()
+    UsersRouters(),
+    MenuRouters()
   ];
 
   RoutersConfigure(this._router);
